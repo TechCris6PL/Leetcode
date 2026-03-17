c8afd047102c85c333e3a6b838ee6392ea46d9c7
Cris <krystian10kluz@gmail.com>
2026-03-17 20:52:56 +0100

    Update Longest_Palindormic_Substring5.py
    adding comments

--- Longest_Palindormic_Substring5.py
+++ Longest_Palindormic_Substring5.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def longestPalindrom(self,s):
-        def palindrom(left,right):
+        def palindrom(left,right): # check odd and even palindrom, return it
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 left -= 1
                 right += 1
@@ -8,8 +8,8 @@ class Solution(object):
         MaxRn = s[0]
         for i in range(0,len(s)):
 
-            odd = palindrom(i,i)
-            even = palindrom(i,i+1)
+            odd = palindrom(i,i) # expand in both sides
+            even = palindrom(i,i+1) # expand in both but let right be +1 more than i and left, for even
             if len(odd) > len(MaxRn):
                 MaxRn = odd
             if len(even) > len(MaxRn):
